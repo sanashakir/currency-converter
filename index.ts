abc15a2cad5813ca80c0178db450a7dd984e0602
Sana Shakir <sanashakir105@gmail.com>
2024-08-16 18:44:30 +0500

    push

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let currency: any = {
+  USD: 1,
+  JPY: 109.45,
+  GBP: 0.82,
+  EUR: 0.92,
+  PKR: 277.91,
+};
+
+let currencyAns = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "JPY", "GBP", "EUR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "JPY", "GBP", "EUR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number",
+  },
+]);
+
+let fromAmount = currency[currencyAns.from];
+let toAmount = currency[currencyAns.to];
+let userAmount = currencyAns.amount;
+let baseCurrency = userAmount / fromAmount;
+let result = baseCurrency * toAmount;
+console.log(result);
